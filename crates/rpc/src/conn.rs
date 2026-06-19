eac04fe26740e17119bac3e25da598644f7f8beb
Jay K <116842461+xjayk@users.noreply.github.com>
2026-06-19 10:21:32 -0400

    Ignore tests that depend on deleted language configs (#38)
    * Refine CI commands by removing error suppression
    
    Removed '|| true' from Clippy and Tests commands for better error handling.
    
    * Remove tooling, simplify feature flags & deps
    
    Remove tooling workspace members and delete several tooling/license symlinks; prune xtask/compliance/perf tool crates. Simplify feature_flags crate: condense trait implementations, shrink FeatureFlagStore API and internals, remove settings/overrides plumbing and many helper types/macros. Change crashes::init to an async signature that returns Arc<Client> directly. Update workspace manifest and Cargo.lock to reflect dependency cleanup and bump Windows-related crates (e.g. windows-sys -> 0.61.2) along with removal of many now-unused packages.
    
    * Ignore tests that depend on deleted language configs

--- crates/rpc/src/conn.rs
+++ crates/rpc/src/conn.rs
@@ -1,24 +1,11 @@
-#![allow(unused_imports)]
-use std::sync::Arc;
-use std::sync::atomic::AtomicBool;
-
 pub struct Connection;
 
 impl Connection {
-    pub fn new<S>(_stream: S) -> Self
-    where
-        S: 'static + Send + Unpin + futures::Sink<async_tungstenite::tungstenite::Message, Error = anyhow::Error>
-            + futures::Stream<Item = anyhow::Result<async_tungstenite::tungstenite::Message>>,
-    {
+    pub fn new<S>(_stream: S) -> Self where S: 'static + Send + Unpin + futures::Sink<async_tungstenite::tungstenite::Message, Error = anyhow::Error> + futures::Stream<Item = anyhow::Result<async_tungstenite::tungstenite::Message>> {
         Self
     }
 
     pub async fn send(&mut self, _message: async_tungstenite::tungstenite::Message) -> anyhow::Result<()> {
         Ok(())
-    }
-
-    #[cfg(any(test, feature = "test-support"))]
-    pub fn in_memory(_executor: gpui::BackgroundExecutor) -> (Self, Self, Arc<AtomicBool>) {
-        (Self, Self, Arc::new(AtomicBool::new(false)))
     }
 }
--- crates/rpc/src/proto_client.rs
+++ crates/rpc/src/proto_client.rs
@@ -1,168 +1,23 @@
-#![allow(unused_variables)]
 use std::sync::Arc;
-use anyhow::Result;
-use futures::future::{BoxFuture, LocalBoxFuture};
-use futures::stream::BoxStream;
-use collections::{HashMap, TypeIdHashMap};
-use gpui::{AnyEntity, AnyWeakEntity, AsyncApp, Entity};
-use parking_lot::Mutex;
 
-pub trait ProtoClient: Send + Sync {
-    fn request(
-        &self,
-        envelope: proto::Envelope,
-        request_type: &'static str,
-    ) -> BoxFuture<'static, Result<proto::Envelope>>;
+pub trait ProtoClient: Send + Sync {}
 
-    fn request_stream(
-        &self,
-        envelope: proto::Envelope,
-        request_type: &'static str,
-    ) -> BoxFuture<'static, Result<BoxStream<'static, Result<proto::Envelope>>>>;
-
-    fn send(&self, envelope: proto::Envelope, message_type: &'static str) -> Result<()>;
-
-    fn send_response(&self, envelope: proto::Envelope, message_type: &'static str) -> Result<()>;
-
-    fn message_handler_set(&self) -> &Mutex<ProtoMessageHandlerSet>;
-
-    fn is_via_collab(&self) -> bool;
-    fn has_wsl_interop(&self) -> bool;
-}
-
-pub enum EntityMessageSubscriber {
-    Entity { handle: AnyWeakEntity },
-    Pending(Vec<Box<dyn proto::AnyTypedEnvelope>>),
-}
-
-pub type ProtoMessageHandler = Arc<
-    dyn Send + Sync + Fn(AnyEntity, Box<dyn proto::AnyTypedEnvelope>, AnyProtoClient, AsyncApp) -> LocalBoxFuture<'static, Result<()>>
->;
-
-pub struct ProtoMessageHandlerSet {
-    pub entity_types_by_message_type: TypeIdHashMap<std::any::TypeId>,
-    pub entities_by_type_and_remote_id: HashMap<(std::any::TypeId, u64), EntityMessageSubscriber>,
-    pub entity_id_extractors: TypeIdHashMap<fn(&dyn proto::AnyTypedEnvelope) -> u64>,
-    pub entities_by_message_type: TypeIdHashMap<AnyWeakEntity>,
-    pub message_handlers: TypeIdHashMap<ProtoMessageHandler>,
-}
-
-impl ProtoMessageHandlerSet {
-    pub fn clear(&mut self) {
-        self.entity_types_by_message_type.clear();
-        self.entities_by_type_and_remote_id.clear();
-        self.entity_id_extractors.clear();
-        self.entities_by_message_type.clear();
-        self.message_handlers.clear();
-    }
-
-    pub fn handle_message(this: &Mutex<Self>, message: Box<dyn proto::AnyTypedEnvelope>, client: AnyProtoClient, cx: AsyncApp) -> Option<LocalBoxFuture<'static, Result<()>>> {
-        None
-    }
-}
-
-impl Default for ProtoMessageHandlerSet {
-    fn default() -> Self {
-        Self {
-            entity_types_by_message_type: TypeIdHashMap::default(),
-            entities_by_type_and_remote_id: HashMap::default(),
-            entity_id_extractors: TypeIdHashMap::default(),
-            entities_by_message_type: TypeIdHashMap::default(),
-            message_handlers: TypeIdHashMap::default(),
-        }
-    }
-}
-
-#[derive(Clone)]
-pub struct AnyProtoClient(pub Arc<dyn ProtoClient>);
+pub struct AnyProtoClient;
 
 impl AnyProtoClient {
-    pub fn new<T: ProtoClient + 'static>(client: Arc<T>) -> Self {
-        Self(client)
+    pub fn new<T: ProtoClient + 'static>(_client: Arc<T>) -> Self {
+        Self
     }
 
-    pub fn is_via_collab(&self) -> bool {
-        self.0.is_via_collab()
-    }
-
-    pub fn has_wsl_interop(&self) -> bool {
-        self.0.has_wsl_interop()
-    }
-
-    pub fn request<T: proto::RequestMessage>(
-        &self,
-        _request: T,
-    ) -> impl std::future::Future<Output = Result<T::Response>> + 'static {
-        async move { Err(anyhow::anyhow!("RPC stubbed out")) }
-    }
-
-    pub fn send<T: proto::EnvelopedMessage>(&self, _request: T) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn send_response<T: proto::EnvelopedMessage>(&self, _request_id: u32, _request: T) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn add_request_handler<M, E, H, F>(&self, _entity: gpui::WeakEntity<E>, _handler: H)
-    where
-        M: proto::RequestMessage,
-        E: 'static,
-        H: 'static + Sync + Fn(gpui::Entity<E>, proto::TypedEnvelope<M>, AsyncApp) -> F,
-        F: std::future::Future<Output = Result<M::Response>> + 'static {}
-
-    pub fn add_entity_message_handler<M, E, H, F>(&self, _handler: H)
-    where
-        M: proto::EnvelopedMessage,
-        E: 'static,
-        H: 'static + Sync + Send + Fn(gpui::Entity<E>, proto::TypedEnvelope<M>, AsyncApp) -> F,
-        F: std::future::Future<Output = Result<()>> + 'static {}
-
-    pub fn subscribe_to_entity<E: 'static>(&self, _remote_id: u64, _entity: &gpui::Entity<E>) {}
+    pub fn is_via_collab(&self) -> bool { false }
 }
 
-impl<T: ProtoClient + 'static> From<Arc<T>> for AnyProtoClient {
-    fn from(client: Arc<T>) -> Self {
-        Self(client)
-    }
-}
-
-pub struct NoopProtoClient {
-    handler_set: Mutex<ProtoMessageHandlerSet>,
-}
+pub struct NoopProtoClient;
 
 impl NoopProtoClient {
     pub fn new() -> Arc<Self> {
-        Arc::new(Self { handler_set: Mutex::new(ProtoMessageHandlerSet::default()) })
+        Arc::new(Self)
     }
 }
 
-impl ProtoClient for NoopProtoClient {
-    fn request(&self, _: proto::Envelope, _: &'static str) -> BoxFuture<'static, Result<proto::Envelope>> {
-        Box::pin(async { Err(anyhow::anyhow!("stubbed")) })
-    }
-
-    fn request_stream(&self, _: proto::Envelope, _: &'static str) -> BoxFuture<'static, Result<BoxStream<'static, Result<proto::Envelope>>>> {
-        Box::pin(async { Err(anyhow::anyhow!("stubbed")) })
-    }
-
-    fn send(&self, _: proto::Envelope, _: &'static str) -> Result<()> {
-        Ok(())
-    }
-
-    fn send_response(&self, _: proto::Envelope, _: &'static str) -> Result<()> {
-        Ok(())
-    }
-
-    fn message_handler_set(&self) -> &Mutex<ProtoMessageHandlerSet> {
-        &self.handler_set
-    }
-
-    fn is_via_collab(&self) -> bool {
-        false
-    }
-
-    fn has_wsl_interop(&self) -> bool {
-        false
-    }
-}
+impl ProtoClient for NoopProtoClient {}
